b51d99102eefaa682c6cb6e619f48fb9600f0195
Richard Bomfim <r.bomfim007@gmail.com>
2021-04-22 16:03:26 -0300

    Programa de salarios

---
+++ NewProject/Entities/outsourcedEmployee.cs
@@ -0,0 +1,24 @@
+﻿namespace NewProject.Entities
+{
+    class outsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public outsourcedEmployee()
+        {
+
+        }
+
+        public outsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
